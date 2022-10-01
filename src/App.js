4bcd3d11c8e8e8ac4d1a86c243397f9c74df82ee
MelvinGesulgon <freelancemgesulgon@gmail.com>
2022-10-01 17:14:12 +0800

    added folders for img and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import '/node_modules/bootstrap/dist/css/bootstrap.css'
 import Navbar from './components/navbar';
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+// import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 function App() {
   return (
