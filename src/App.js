35b784614981906f0ccfbe675182e2d792c65f68
MelvinGesulgon <freelancemgesulgon@gmail.com>
2022-10-01 16:59:31 +0800

    test content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import '/node_modules/bootstrap/dist/css/bootstrap.css'
+import Navbar from './components/navbar';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <button className="btn btn-danger">red button</button>
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mb-3 mt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius officia quia at? Harum aliquid expedita doloremque natus possimus nemo eveniet!</div>
+          
+          <div className="col-md-6 mb-3 mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem harum ducimus eveniet nemo eos, consequatur eum mollitia molestias molestiae sunt id eligendi soluta obcaecati quibusdam officia ea, vel nihil necessitatibus?</div>
+        </div>
+      </div>
+        
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (  
+        <div className="d-flex justify-content-space-between">
+            <ul>
+                <li>Home</li>
+                <li>Product</li>
+                <li>Services</li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default Navbar;
