d07806b2da563cde6b44634f5f61bf8db62668b6
Ben <benjamentorres@gmail.com>
2022-10-10 16:51:55 +0800

    fixed footer and nav (to be edit)

---
+++ src/components/subcom/NAVBAR.js
@@ -0,0 +1,35 @@
+import '../css/navbar.css'
+import {Link} from 'react-router-dom';
+import Logo from '../images/logo.png'
+
+const NavbarMain = () => {
+    return ( 
+        <div className="navbar">
+            <div className="brand">
+                <img src={Logo} alt="" />
+                <h1>Access Success</h1>
+            </div>
+            <div className="pages">
+                <ul>
+                    <li>
+                        <Link to= '/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/search'>Search</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/login'>Login</Link>
+                    </li>
+                    <li id="signupbtn">
+                        <Link to='/signup'>Signup</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+     );
+}
+ 
+export default NavbarMain;
