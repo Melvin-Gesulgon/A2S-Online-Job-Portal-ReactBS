a062536bf49b77ce7a6ee82e7ad44709086587c8
MelvinGesulgon <freelancemgesulgon@gmail.com>
2022-10-15 10:32:09 +0800

    updated files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import '/node_modules/bootstrap/dist/css/bootstrap.css';
-import SignUp from './pages/signup';
-
+import JobPostTemplate from './pages/JobPostTemplate';
 
 function App() {
   return (
     <div className="App">
        
-       <SignUp />
-     
+    <JobPostTemplate
+    position="Manager"
+    companyName="Test Company"
+    location="Caloocan"
+    salary="PhP 35,000"
+    jobDescription1="Manage the efficient workflow of staff and maximize the earning potential of the company."
+    jobDescription2="He/She will work with marketing department to generate more leads from the campaign."
+    companyOverview1="Test Company was founded by  brothers Test MyCode and Debug MyCode in 2000 to provide IT solutions to emerging companies across various fields."
+    companyOverview2="The company has grown from a small agency to a multinational company that caters to both local and foreign clients across the world."
+    
+    />
               
     </div>
   );
---
+++ src/pages/JobPostTemplate.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../css/melvin.css';
+import '../css/main.css';
+
+const JobPostTemplate = (post) => {
+  return (
+
+    <div class="container" id="jpost">
+        <div id="jpost-head">
+            <h2>{post.position}</h2>
+            <h3>{post.companyName}</h3>
+            <br />
+            <p>{post.location}</p>
+            <p>{post.salary}</p>
+            <br />
+            <p>Posted 3 days ago</p>
+            <div id="jpost-appply">
+                <a href="apply.html"class="orangebutton regular nlw">Apply Now</a>
+            </div>
+        </div>
+        <div id="line-break"></div>
+        <div id="jpost-jobdes">
+            <h4>Job Description</h4>
+            <br />
+            <p>{post.jobDescription1}</p>
+            <p>{post.jobDescription2}</p>
+        </div>    
+        <div id="jpost-company">
+        <div id="line-break"></div>
+        
+            <h4>Company Overview</h4>
+            <br />
+            <p>{post.companyOverview1}</p>
+            <p>{post.companyOverview2}</p>
+        </div>
+        <div id="line-break"></div>
+        <a href="../#" class="jobs-link">More jobs from this employer</a>
+        <div id="jpost-save">
+            <button class="graybutton regular">Save this Post</button>
+        </div>
+        <a href="../#" class="jobs-link">Report this job ad</a>
+    </div>
+
+  )
+}
+
+export default JobPostTemplate
