b8ed3c56337100497b2f5e8e3084f236c3625332
MelvinGesulgon <freelancemgesulgon@gmail.com>
2022-10-18 02:53:07 +0800

    removed possible conflict issues in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '/node_modules/bootstrap/dist/css/bootstrap.css';
-import JobPostTemplate from './components/subcomp/JobPostTemplate';
+
 
 function App() {
   return (
